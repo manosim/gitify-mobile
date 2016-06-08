5cfe04fd8290777b7cea9c69a1524e922e24ddaf
Emmanouil Konstantinidis <manos@dabapps.com>
2016-06-08 14:07:14 +0100

    Fix rootReducer

--- App/Reducers/index.js
+++ App/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import * as storage from 'redux-storage';
+import merger from 'redux-storage-merger-immutablejs';
 
 import auth from './Auth';
 import notifications from './Notifications';
 
-export default combineReducers({
+export default storage.reducer(combineReducers({
   auth,
   notifications
-});
+}), merger);
