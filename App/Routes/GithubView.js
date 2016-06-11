f2512c259e96ad50c6a91495e9a6de642f38eb6d
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-06-11 02:21:10 +0100

    Style browser toolbar

--- App/Routes/GithubView.js
+++ App/Routes/GithubView.js
@@ -1,10 +1,44 @@
 import React, { Component, PropTypes } from 'react'; // eslint-disable-line no-unused-vars
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Constants from '../Utils/Constants';
 
 import {
+  StyleSheet,
+  View,
   WebView,
 } from 'react-native';
 
-// import Constants from '../Utils/Constants';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  toolbar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: Constants.TOOLBAR_BG,
+  },
+  toolbarIcon: {
+    marginHorizontal: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    fontSize: 24,
+    color: '#FFF'
+  },
+  toolbarLeft: {
+    flex: .5,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+  toolbarRight: {
+    flex: .5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  }
+});
 
 export default class GithubView extends Component {
   static propTypes = {
@@ -13,10 +47,23 @@ export default class GithubView extends Component {
 
   render() {
     return (
-      <WebView
-        source={{uri: this.props.url}}
-        automaticallyAdjustContentInsets={true}
-        startInLoadingState={true} />
+      <View style={styles.container}>
+        <WebView
+          source={{uri: this.props.url}}
+          automaticallyAdjustContentInsets={true}
+          startInLoadingState={true} />
+
+        <View style={styles.toolbar}>
+          <View style={styles.toolbarLeft}>
+            <Icon name="chevron-left" style={styles.toolbarIcon} />
+            <Icon name="chevron-right" style={styles.toolbarIcon} />
+          </View>
+
+          <View style={styles.toolbarRight}>
+            <Icon name="refresh" style={styles.toolbarIcon} />
+          </View>
+        </View>
+      </View>
     );
   }
 };
